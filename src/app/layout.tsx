4bfdbd731ba642f974194c7abd79ee697404b753
jeonghyun <kdsocks5@naver.com>
2024-02-04 08:08:06 +0900

    analytics speedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,13 @@ import { theme } from "@/components/common/dropDown/themeBtn";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "JH's Stack Blog",
+  title: "대학생의 블로그",
   description: "대학생의 반도체/개발 스택 블로그",
 };
 
@@ -17,6 +19,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme={theme}>
+      <Analytics />
+      <SpeedInsights />
       <body className={inter.className}>{children}</body>
     </html>
   );
