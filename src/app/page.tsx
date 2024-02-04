f97fb709ddaf711be97c5e9ddc67f5b39efcce4a
jeonghyun <kdsocks5@naver.com>
2024-02-04 09:39:09 +0900

    router

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import List from "@/components/common/list/list";
+import { Pagination } from "@/components/common/pagination/pagination";
+import { Layout } from "@/components/layout/Layouts";
+import { posts } from "@/constants/posts";
+import { usePage } from "@/hooks/usePage";
+import { useParams, useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  const currentPosts = usePage(posts, 5, 1);
+  //   router.push("1");
+  return (
+    <Layout>
+      <div className="w-full mx-auto max-w-2xl">
+        <div className="grid justify-items-stretch gap-6">
+          {currentPosts.map((post: any) => (
+            <List
+              key={post._id}
+              date={post.date}
+              title={post.title}
+              description={post.description}
+              imageSrc={post.image}
+              category={post.category}
+              url={post.url}
+            />
+          ))}
+        </div>
+        <div className="flex justify-center mt-12">
+          <Pagination currentPage={1} />
+        </div>
+      </div>
+    </Layout>
+  );
+}
--- src/components/common/pagination/pc.tsx
+++ src/components/common/pagination/pc.tsx
@@ -5,17 +5,20 @@ export function PcPagination({ currentPage }: { currentPage: number }) {
   const totalPages = Math.ceil(posts.length / 5);
   return (
     <div className="join">
-      {Array.from({ length: totalPages }, (_, i) => (
-        <Link href={`/${i + 1}`} key={i}>
-          <input
-            className="join-item btn btn-square"
-            type="radio"
-            name="options"
-            aria-label={String(i)}
-            checked={i + 1 === currentPage}
-          />
-        </Link>
-      ))}
+      {Array.from({ length: totalPages }, (_, i) => {
+        console.log("INDEX", i);
+        return (
+          <Link href={`/${i + 1}`} key={i}>
+            <input
+              className="join-item btn btn-square"
+              type="radio"
+              name="options"
+              aria-label={String(i + 1)}
+              checked={i + 1 === currentPage}
+            />
+          </Link>
+        );
+      })}
     </div>
   );
 }
